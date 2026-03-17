a66de04048bff3c8ea62e2c8333da3d694eb172c
Aradhya Singh <aradhya301@gmail.com>
2026-03-17 14:07:44 +0530

    Time: 87 ms (82.43%), Space: 28.5 MB (61.22%) - LeetHub

--- 0739-daily-temperatures/0739-daily-temperatures.py
+++ 0739-daily-temperatures/0739-daily-temperatures.py
@@ -10,3 +10,26 @@ class Solution:
 
             stack.append(i)     
         return result
+
+# dry run
+# stack = [0]
+# prev = 0
+# res[0] = 1-0
+
+# stack = [1]
+# prev = 1
+# res[1] = 2-1
+
+# stack =[2,3,4]
+# prev = 4
+# res[4] = 5-4
+# prev = 3
+# res[3] = 5-3 =2
+
+# stack = 2,5
+
+# prev = 5
+# res[5] = 6-5 = 1
+# res[2] = 6-2 = 4
+
+# stack = [6]
